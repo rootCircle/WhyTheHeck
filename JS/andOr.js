78b47092b16081bce5ac2f598bb2c1e477cd0349
Akhilender Bongirwar <112749383+akhilender-bongirwar@users.noreply.github.com>
2023-09-26 15:34:59 +0530

    Update andOr.js

--- JS/andOr.js
+++ JS/andOr.js
@@ -2,6 +2,6 @@ let isFun = true;
 let isLearning = 0;
 
 console.log(isFun && "Learning is fun!");
-console.log(isFun || "Learning is not fun!");
+console.log(isLearning || "Learning is not fun!");
 
 // try the same above by making isFun = false and isLearning = 1
