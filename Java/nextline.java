c49c5fcb922e9f36de4053f619c5abec00d538f0
Aman <amansheo@gmail.com>
2023-10-11 00:01:31 +0530

    Reverted file name

--- Java/nextline.java
+++ Java/nextline.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Nextline {
+public class nextline {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Scanner is used to take input in java
         System.out.println("Enter a number :"); // Used to Print a Statement
