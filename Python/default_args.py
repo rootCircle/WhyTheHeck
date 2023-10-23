fcf2faa409227b566f08de94dbb1804d450ae2f8
UditJain2622004 <uditjain2622004@gmail.com>
2023-10-23 22:40:35 +0530

    add: confusing short-curcuiting and default args. in python

---
+++ Python/default_args.py
@@ -0,0 +1,6 @@
+def add_item(item, lst=[]):
+    lst.append(item)
+    return lst
+list1 = add_item(1)
+list2 = add_item(2)
+print(list1)
---
+++ Python/short_circuiting.py
@@ -0,0 +1,4 @@
+result = "First" or "Second"
+result2 = "First" and "Second"
+print(result)
+print(result2)
